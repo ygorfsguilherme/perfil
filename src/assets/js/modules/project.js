3247f706d594ee7158711440a74c8b66fe0cb5e3
Ygor Guilherme <ygorfsguilherme@gmail.com>
2022-06-13 22:40:43 -0300

    added project

--- src/assets/js/modules/project.js
+++ src/assets/js/modules/project.js
@@ -1,45 +1,57 @@
-let projectArea = document.querySelector('[data-projects]');
+let projectArea = document.querySelector("[data-projects]");
 
 const projects = [
   {
-    image: 'src/assets/img/project/project-criptdex.png',
-    title: 'Criptdex',
-    description: 'Codificador de texto, que substitui parte da palavra por outra codificação, dificultando o entendimento.',
-    github_link: 'https://github.com/ygorfsguilherme/criptdex',
-    view_app: 'https://ygorfsguilherme.github.io/criptdex/',
+    image: "src/assets/img/project/project-criptdex.png",
+    title: "Criptdex",
+    description:
+      "É um codificador de texto, a sua utilização é fazer com que frases seja indecifrável.",
+    github_link: "https://github.com/ygorfsguilherme/criptdex",
+    view_app: "https://ygorfsguilherme.github.io/criptdex/",
   },
 
   {
-    image: 'src/assets/img/project/project-jogo-da-forca.png',
-    title: 'Jogo da forca', 
-    description: 'Um breve descrição sobre o project, de forma simple e clara',
-    github_link: 'https://github.com/ygorfsguilherme/jogo-da-forca',
-    view_app: 'https://ygorfsguilherme.github.io/jogo-da-forca/',
+    image: "src/assets/img/project/project-jogo-da-forca.png",
+    title: "Jogo da forca",
+    description:
+      "Um simples jogo da forca, onde você pode tentar adivinhar uma palavra.",
+    github_link: "https://github.com/ygorfsguilherme/jogo-da-forca",
+    view_app: "https://ygorfsguilherme.github.io/jogo-da-forca/",
   },
 
   {
-    image: 'src/assets/img/project/portfolio.png',
-    title: 'Portfolio',
-    description : 'Foi um dos desafio do programa ONE da oracle e alura, afim de colocar todos os meus projetos em um só lugar.',
-    github_link: 'https://github.com/ygorfsguilherme/portfolio',
-    view_app: 'https://ygorfsguilherme.github.io/portfolio/',
+    image: "src/assets/img/project/project-portfolio.png",
+    title: "Portfolio",
+    description:
+      "Local em que eu possa colocar meus principais projetos além de informações e contatos sobre mim",
+    github_link: "https://github.com/ygorfsguilherme/portfolio",
+    view_app: "https://ygorfsguilherme.github.io/portfolio/",
   },
 
   {
-    image: 'src/assets/img/project/project-pyformail.png',
-    title: 'Pyformail',
-    description: 'Uma api que envia informações de um formulario para um email, criado para envia informações de contato.',
-    github_link: 'https://github.com/ygorfsguilherme/api-pyformail',
-    view_app: 'https://github.com/ygorfsguilherme/api-pyformail',
+    image: "src/assets/img/project/project-pyformail.png",
+    title: "Pyformail",
+    description:
+      "Uma api que envia informações de um formulario para um email, criado para envia informações de contato.",
+    github_link: "https://github.com/ygorfsguilherme/api-pyformail",
+    view_app: "https://github.com/ygorfsguilherme/api-pyformail",
   },
   {
-    image: 'src/assets/img/project/project-alura-geek.png',
-    title: 'Alura Geek',
-    description: 'Alura Geek é uma e-commerce que une uma gama de itens da cultura geek.',
-    github_link: 'https://github.com/ygorfsguilherme/alura-geek',
-    view_app: 'https://github.com/ygorfsguilherme/alura-geek',
+    image: "src/assets/img/project/project-alura-geek.png",
+    title: "Alura Geek",
+    description: "Um e-commerce que une uma gama de itens da cultura geek.",
+    github_link: "https://github.com/ygorfsguilherme/alura-geek",
+    view_app: "https://ygorfsguilherme.github.io/alura-geek/",
   },
-]
+  {
+    image: "src/assets/img/project/project-gmoney.png",
+    title: "GMoney - Conversor",
+    description:
+      "é um conversor de moeda sendo possivel converter para alguns tipos de moeda como dólar e real.",
+    github_link: "https://github.com/ygorfsguilherme/gmoney-conversor",
+    view_app: "https://github.com/ygorfsguilherme/gmoney-conversor",
+  },
+];
 
 function createProject() {
   const newProject = projects.map((project) => {
@@ -61,7 +73,7 @@ function createProject() {
     </div>`;
   });
 
-  return newProject.join('');
+  return newProject.join("");
 }
 
-projectArea.insertAdjacentHTML('afterbegin', createProject());
+projectArea.insertAdjacentHTML("afterbegin", createProject());
